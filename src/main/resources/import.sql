f420e1e10ccfcc20886954daab89483ec3930d15
Ruben Manuel <rubensapalo97@gmail.com>
2025-04-06 21:47:50 +0100

    Spring Security checklist, not tested

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$qvr4m8avPsMQdN5Kg9MpAu1L.jbr0CwPTfwmAv1i3LDDzzJbu8.P2');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$qvr4m8avPsMQdN5Kg9MpAu1L.jbr0CwPTfwmAv1i3LDDzzJbu8.P2');
 
 
 INSERT INTO tb_role(authority) VALUES ('ROLE_OPERATOR');
