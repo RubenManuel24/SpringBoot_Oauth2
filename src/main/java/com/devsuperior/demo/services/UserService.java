4ef93ee490c42d2ed009bd290836618cfaee2bce
Ruben Manuel <rubensapalo97@gmail.com>
2025-04-10 23:27:33 +0100

    Access control by role, by route

--- src/main/java/com/devsuperior/demo/services/UserService.java
+++ src/main/java/com/devsuperior/demo/services/UserService.java
@@ -1,12 +1,12 @@
 package com.devsuperior.demo.services;
 import java.util.List;
+import com.devsuperior.demo.entities.Role;
+import com.devsuperior.demo.entities.User;
+import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-import com.devsuperior.demo.entities.Role;
-import com.devsuperior.demo.entities.User;
 import com.devsuperior.demo.projections.UserDetailsProjection;
 import com.devsuperior.demo.repositories.UserRepository;
 
